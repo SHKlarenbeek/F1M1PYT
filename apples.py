c28d74881fb97529ee10d7dac604a1ad6cb80c0e
SHKlarenbeek <92087200+SHKlarenbeek@users.noreply.github.com>
2021-10-12 12:50:46 +0200

    Add files via upload

---
+++ apples.py
@@ -0,0 +1,25 @@
+print("eerst delen we de 333 bomen door 3")
+print(333 / 3)
+print("dan doen we dat getal keer 2")
+print(111 * 2)
+print("dan bereken we hoeveel appels je krijgt van bomen uit de schaduw")
+print(146 / 100 * 80)
+print("dan doen we de hoeveelheid appels van schaduw tegen de bomen in de schaduw")
+print(116.8 * 222)
+print("en doen we ook de appels van de bomen in de zon")
+print(111 * 146)
+print("dan doen we de hoeveelheiden bij elkaar")
+print(25929.6 + 16206)
+print("en dan delen we dat door 3")
+print(42135.6 / 3)
+print("dus in totaal krijg je 14045 appels")
+
+
+
+
+
+
+
+
+
+
