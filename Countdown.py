077471d73b1f3e4a11f79a4e87e9a77dc1bcec61
SHKlarenbeek <34334@ma-web.nl>
2021-10-29 09:25:30 +0200

    geupdate

--- Countdown.py
+++ Countdown.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 i = 1000
 while i > -1:
     print(i)
@@ -6,12 +6,5 @@ while i > -1:
 if i < 0:
     print("Never gonna give you up, Never gonna let you down Never, gonna run around and desert you")
 
-=======
-i = 1000
-while i > -1:
-    print(i)
-    i -= 1
-if i < 0:
-    print("Never gonna give you up, Never gonna let you down Never, gonna run around and desert you")
 
->>>>>>> cd5d290d6e7b91b86ebfb8c0d087ef3423f7e78e
+
